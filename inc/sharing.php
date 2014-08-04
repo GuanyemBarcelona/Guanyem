eb26c3c5a38a6af5228ff7f6400a20c2425d0b6d
Felip Ladron <fladron@omitsis.com>
2014-08-04 17:04:41 +0200

    Arreglar url para listado de posts

--- inc/sharing.php
+++ inc/sharing.php
@@ -1,11 +1,16 @@
+<?php
+$current_url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$current_title = single_post_title('', FALSE);
+$message = $current_title . '%20' . $current_url;
+?>
 <div class="sharing-buttons">
   <h2><?php //_('Share','Guanyem'); ?>Compartir</h2>
   <ul>
     <li>
-      <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink($post->ID); ?>" data-action="share-facebook"><i class="fa fa-facebook-square"></i><span>facebook</span></a>
+      <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $current_url; ?>" data-action="share-facebook"><i class="fa fa-facebook-square"></i><span>facebook</span></a>
     </li>
     <li>
-      <a href="http://twitter.com/home?status=<?php echo get_the_title($post->ID) . '%20' . get_permalink($post->ID); ?>" data-action="share-twitter"><i class="fa fa-twitter-square"></i><span>twitter</span></a>
+      <a href="http://twitter.com/home?status=<?php echo $message; ?>" data-action="share-twitter"><i class="fa fa-twitter-square"></i><span>twitter</span></a>
     </li>
   </ul>
 </div>
