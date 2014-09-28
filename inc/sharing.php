71eb6e1763401d3c4f829a95f7ccb5e60fd1f280
Felip Ladron <fladron@gmail.com>
2014-09-28 19:27:23 +0200

    Arreglado share twitter
    Faltaba indicar urlencode. demás, se ha incluido "via @guanyem" en el
    texto a compartir en twitter

--- inc/sharing.php
+++ inc/sharing.php
@@ -1,7 +1,7 @@
 <?php
 $current_url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $current_title = single_post_title('', FALSE);
-$message = $current_title . '%20' . $current_url;
+$twitter_text = urlencode($current_title . ' ' . $current_url . ' via @guanyem');
 ?>
 <div class="sharing-buttons">
   <h2><?php _e('Share', 'guanyem'); ?></h2>
@@ -10,7 +10,7 @@ $message = $current_title . '%20' . $current_url;
       <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $current_url; ?>" data-action="share-facebook"><i class="fa fa-facebook-square"></i><span>facebook</span></a>
     </li>
     <li>
-      <a href="http://twitter.com/home?status=<?php echo $message; ?>" data-action="share-twitter"><i class="fa fa-twitter-square"></i><span>twitter</span></a>
+      <a href="http://twitter.com/home?status=<?php echo $twitter_text; ?>" data-action="share-twitter"><i class="fa fa-twitter-square"></i><span>twitter</span></a>
     </li>
   </ul>
 </div>
