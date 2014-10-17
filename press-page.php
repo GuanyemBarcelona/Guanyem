e6b8e36046ab20f27861865eb742dd6fc7cb9d1d
Felip Ladron <fladron@gmail.com>
2014-10-17 17:58:43 +0200

    Press page con sidebar left con nube tags

--- press-page.php
+++ press-page.php
@@ -11,10 +11,21 @@ Template Name: Press Page
     <div class="container" role="main">
         <div class="<?php echo tc__f( 'tc_column_content_wrapper_classes' , 'row column-content-wrapper' ) ?>">
 
-            <?php do_action( '__before_article_container'); ##hook of left sidebar?>
+                <div class="span3 left tc-sidebar">
+                  <div id="left" class="widget-area" role="complementary">
+                    <aside id="press-tags" class="widget widget_tag_cloud">
+                      <h3 class="widget-title"><?php _e("Press tag cloud", "guanyem"); ?></h3>
+                      <?php wp_tag_cloud(array('taxonomy'=>'press_tag')); ?>
+                    </aside>
+                  </div><!-- #left -->
+                </div>
+
+                <?php do_action( '__before_article_container'); ##hook of left sidebar?>
                 
-                <div id="content" class="<?php echo tc__f( '__screen_layout' , tc__f ( '__ID' ) , 'class' ) ?> article-container">
-                    
+                <div id="content" class="<?php echo tc__f( '__screen_layout' , tc__f ( '__ID' ) , 'class' ) ?> span9 article-container press-list-container">
+
+                    <?php// include 'inc/sharing.php'; ?>
+
                     <?php do_action ('__before_loop');##hooks the header of the list of post : archive, search... ?>
 
                         <?php query_posts( 'post_type=press'); ?>
@@ -37,7 +48,7 @@ Template Name: Press Page
 
                 </div><!--.article-container -->
 
-           <?php do_action( '__after_article_container'); ##hook of left sidebar ?>
+                <?php do_action( '__after_article_container'); ##hook of left sidebar ?>
 
         </div><!--.row -->
     </div><!-- .container role: main -->
